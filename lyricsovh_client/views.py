4d24e4bf239d0547408475dd71d4607594e69ea1
garethhu <garethjhumphries@gmail.com>
2021-03-08 08:42:18 +0000

    Added rest endpoint for getting a song using lyricsovh client, dummied out currently

--- lyricsovh_client/views.py
+++ lyricsovh_client/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
+
+@api_view(['GET'])
+def get_song(request, artist, title):
+    response_obj = {
+        "artist": artist,
+        "title": title
+    }
+    try:
+        song = []
+        response_obj['lyrics'] = song.lyrics
+    except Exception as e:
+        response_obj['error_msg'] = 'An error occurred getting song from artist'
+        response_obj['exception'] = e
+
+    return Response(response_obj)
